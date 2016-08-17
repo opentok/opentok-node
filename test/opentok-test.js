c0417c3ca1f13cfd9c3b8435b740898b0192eca3
Dave Mun <dmun@tokbox.com>
2016-08-16 19:19:43 -0700

    Refactor to make dial method of opentok.js

--- test/opentok-test.js
+++ test/opentok-test.js
@@ -5,7 +5,7 @@ var expect = require('chai').expect,
 // Subject
 var OpenTok = require('../lib/opentok.js'),
     Session = require('../lib/session.js'),
-    SipInterconnect = require('../lib/sipInterconnect.js')._constructor_,
+    SipInterconnect = require('../lib/sipInterconnect.js'),
     pkg = require('../package.json');
 
 // Fixtures
@@ -619,7 +619,7 @@ describe('#dial', function() {
       var scope = nock('https://api.opentok.com:443')
         .matchHeader('x-tb-partner-auth', apiKey+':'+apiSecret)
         .matchHeader('user-agent', new RegExp("OpenTok-Node-SDK\/"+pkg.version))
-        .post('/v2/partner/123456/call;transport=tls', {
+        .post('/v2/partner/123456/call?transport=tls', {
           sessionId: this.sessionId,
           token: this.token,
           sip: {
