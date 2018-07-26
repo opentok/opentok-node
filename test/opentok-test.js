db04694c18b0da6b0bbdc2b83e86f36cf7ca5e74
Jeff Swartz <swartz@tokbox.com>
2018-07-25 17:22:43 -0700

    Adding Session.getStream() method

--- test/opentok-test.js
+++ test/opentok-test.js
@@ -1,5 +1,6 @@
 /* */
 var expect = require('chai').expect;
+var mockStreamRequest = require('./stream-test').mockStreamRequest;
 var nock = require('nock');
 var _ = require('lodash');
 var jwt = require('jsonwebtoken');
@@ -1060,5 +1061,57 @@ describe('#dial', function () {
       function () {
         expect(options).to.deep.equal(optionsUntouched);
       });
+  });
+});
+
+describe('getStream', function () {
+  var opentok = new OpenTok('APIKEY', 'APISECRET');
+  var SESSIONID = '1_MX4xMDB-MTI3LjAuMC4xflR1ZSBKYW4gMjggMTU6NDg6NDAgUFNUIDIwMTR-MC43NjAyOTYyfg';
+  var STREAMID = '4072fe0f-d499-4f2f-8237-64f5a9d936f5';
+
+  afterEach(function () {
+    nock.cleanAll();
+  });
+
+  describe('valid responses', function () {
+    it('should not get an error and get valid stream data given valid parameters', function (done) {
+      mockStreamRequest(SESSIONID, STREAMID);
+      opentok.getStream(SESSIONID, STREAMID, function (err, stream) {
+        expect(err).to.be.null;
+        expect(stream.id).to.equal('fooId');
+        expect(stream.name).to.equal('fooName');
+        expect(stream.layoutClassList.length).to.equal(1);
+        expect(stream.layoutClassList[0]).to.equal('fooClass');
+        expect(stream.videoType).to.equal('screen');
+        done();
+      });
+    });
+
+    it('should return an error if sessionId is null', function (done) {
+      mockStreamRequest(SESSIONID, STREAMID);
+      opentok.getStream(null, STREAMID, function (err, stream) {
+        expect(err).to.not.be.null;
+        expect(stream).to.be.undefined;
+        done();
+      });
+    });
+
+    it('should return an error if streamId is null', function (done) {
+      mockStreamRequest(SESSIONID, STREAMID);
+      opentok.getStream(SESSIONID, null, function (err, stream) {
+        expect(err).to.not.be.null;
+        expect(stream).to.be.undefined;
+        done();
+      });
+    });
+
+    it('should return an error if the REST method returns a 404 response code', function (done) {
+      mockStreamRequest(SESSIONID, STREAMID, 400);
+      opentok.getStream(SESSIONID, STREAMID, function (err, stream) {
+        expect(err).to.not.be.null;
+        expect(stream).to.be.undefined;
+        done();
+      });
+    });
   });
 });
